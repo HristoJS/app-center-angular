626561596c1a2bf77a8c21a025312936fb566a72
Hristo Stoyanov <hristo.stoyanov@scalefocus.com>
2017-04-21 10:33:34 +0300

    Added firebase integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from 'angularfire2';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDS-y4iBRU06Sz35mwogo6IZdBWTEvgHN4',
+  authDomain: 'app-center-3e9f1.firebaseapp.com',
+  databaseURL: 'https://app-center-3e9f1.firebaseio.com',
+  projectId: 'app-center-3e9f1',
+  storageBucket: 'app-center-3e9f1.appspot.com',
+  messagingSenderId: '946954972418'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
